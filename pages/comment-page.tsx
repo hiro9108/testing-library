058565f6014beb6f593b7f389512943c57c953af
hiro9108 <hiroshi.8.egawa@gmail.com>
2021-02-20 08:27:22 -0800

    first commit

---
+++ pages/comment-page.tsx
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout'
+const CommentPage: React.FC = () => {
+  return (
+    <Layout title="comment">
+      <p className="text-4xl">comment page</p>
+    </Layout>
+  )
+}
+export default CommentPage
---
+++ pages/task-page.tsx
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout'
+const TaskPage: React.FC = () => {
+  return (
+    <Layout title="Task">
+      <p className="text-4xl">task page</p>
+    </Layout>
+  )
+}
+export default TaskPage
